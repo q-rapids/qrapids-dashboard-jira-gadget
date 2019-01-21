78ed0be1aa15e165ce13c7c11b915a0bb528f3d0
German Mora <german.eduardo.mora@est.fib.upc.edu>
2019-01-21 22:12:35 +0100

    QRG-009: HttpRequest

--- src/main/java/com/atlassian/plugins/qrapids/rest/message/MyRestResource.java
+++ src/main/java/com/atlassian/plugins/qrapids/rest/message/MyRestResource.java
@@ -5,6 +5,11 @@ import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A resource of message.
@@ -15,12 +20,23 @@ public class MyRestResource {
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getMessage(@QueryParam("key") String key)
-    {
-        if(key!=null)
-            return Response.ok(new MyRestResourceModel(key, getMessageFromKey(key))).build();
-        else
-            return Response.ok(new MyRestResourceModel("defaultaaaaa","Hello World")).build();
+    public Response getMessage(@QueryParam("key") String key) throws IOException {
+        String url = "https://ip.seeip.org/json";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return Response.ok(response).build();
     }
 
     @GET
